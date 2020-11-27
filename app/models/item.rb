fd038fdb663cb46551545206a697952d4c047aff
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-11-27 11:19:37 +0900

    itemモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :cost_beaver_id, presence: true
+  validates :shipment_area_id, presence: true
+  validates :preparation_days_id, presence: true
+  validates :price, presence: true
+
+  # validates :image, presence: true  
+  
+  belongs_to :user
+  # has_one_attached :image
 end
