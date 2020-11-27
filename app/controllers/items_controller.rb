cbcbe975ae4f862c1955041e06ea7afa045b971b
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-11-27 12:02:27 +0900

    未ログイン状態の出品禁止

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,13 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    if user_signed_in?
+      @item = Item.new
+    else
+      redirect_to '/users/sign_in'
+    end
   end
 end
