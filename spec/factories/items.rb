0fe9fdc2bf74f821c3a748f22f9fe292415ea966
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-11-29 12:25:15 +0900

    モデル単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                {Faker::Lorem.word}
+    description         {Faker::Lorem.sentence}
+    category_id         {1}
+    condition_id        {1}
+    cost_beaver_id      {1}
+    shipment_area_id    {1}
+    preparation_days_id {1}
+    price               {"500"}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
