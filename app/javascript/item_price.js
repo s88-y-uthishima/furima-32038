19006b6d848637b8d3019eb0d5747e75b8745828
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-11-29 10:10:17 +0900

    計算のJavaScript追加

---
+++ app/javascript/item_price.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+    const charge = Math.floor(inputValue*0.1);
+    addTaxDom.innerHTML = charge;
+    profitDom.innerHTML = inputValue - charge;
+  });
+});
