afb59a3715737a9c1bc3db3080ee2c3827065687
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-01 19:14:40 +0900

    Formオブジェクト訂正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,20 +1,20 @@
 class OrderForm
   include ActiveModel::Model
  
-  attr_accessor : :user_id, :item_id, :post_code, :prefecture_id, :city, :address_line, :building, :phone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address_line, :building, :phone_number
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/}
-    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :city
     validates :address_line
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/ }
   end
-  validates :building
+  validates :prefecture_id, numericality: { other_than: 0 }
+  # validates :building
 
   def save
-    Purchase.create(user_id: user_id, item_id: item_id)
-    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address_line: address_line, building: building, phone_number: phone_number)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address_line: address_line, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
