d9be7cdc4d1ed5de71ed81974c0ddaf535964432
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-01 11:36:52 +0900

    アクションの定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,16 @@
 class PurchasesController < ApplicationController
+
+  def index
+    @purchases = Purchases.all  
+  end
+
+  def create
+    @purchase = OrderForm.new(set_params)
+    @purchase.save 
+  end
+
+  private
+  def set_params
+    params.permit(:user_id, :item_id, :post_code, :prefecture_id, :city, :address_line, :building, :phone_number)
+  end
 end
