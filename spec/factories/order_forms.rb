c54ea264c4b1212cf3a4942b2e9463038ef5d687
s88-y-uthishima <s88-y-uthishima@users.noreply.github.com>
2020-12-03 19:28:25 +0900

    購入機能テストコード

---
+++ spec/factories/order_forms.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :order_form do
+    token         { "tok_1234567890" }
+    post_code     { '098-6758' }
+    prefecture_id { 1 }
+    city          { '稚内市' }
+    address_line  { '3' }
+    building      { '最北端' }
+    phone_number  { '09012345678' }
+    user_id       { 1 }
+    item_id       { 1 }
+  end
+end
+
